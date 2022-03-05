f9bfaa3412dd6c0f2c50e3043bbd8c2afd6129a4
maykaltenev <michaeltenev@gmail.com>
2022-03-05 21:21:35 +0100

    more loops

--- s19-loops/forLoops/app.js
+++ s19-loops/forLoops/app.js
@@ -16,19 +16,23 @@
 // OUR FIRST FOR LOOP
 // ======================================
 
-for (let i = 1; i <= 30; i++) {
+// for (let i = 1; i <= 30; i++) {
 
-    console.log(i);
-}
+//     console.log(i);
+// }
 
 // ======================================
 // MORE EXAMPLES OF FOR LOOPS!
 // ======================================
-
+// all or odd
 // for (let i = 1; i <= 20; i += 2) {
 //     console.log(i)
 // }
-
+// all even
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log(i)
+// }
+// 100 to 0 by 10
 // for (let i = 100; i >= 0; i -= 10) {
 //     console.log(i);
 // }
---
+++ s19-loops/forLoops/exercise-32-more-loops.js
@@ -0,0 +1,11 @@
+// Write a loop that prints:
+// 25
+// 20
+// 15
+// 10
+// 5
+// 0
+
+for (let index = 25; index >= 0; index -= 5) {
+    console.log(index);
+}
