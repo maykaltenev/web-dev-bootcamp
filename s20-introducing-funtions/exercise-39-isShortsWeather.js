83a03d4f4fa4f91a2fddfea821a45790ce3dd39b
maykaltenev <michaeltenev@gmail.com>
2022-03-06 21:43:53 +0100

    exercise-39-isShortsWeather

---
+++ s20-introducing-funtions/exercise-39-isShortsWeather.js
@@ -0,0 +1,11 @@
+// Write function called isShortsWether
+// It should accept a single number argument, which we will call temperature;
+// If temperature is greater than or equal to 75 Fahrenheit, return true.
+// Otherwise return false.
+function isShortsWeather(temperature) {
+    if (temperature >= 75) {
+        return true;
+    } else {
+        return false;
+    }
+}
