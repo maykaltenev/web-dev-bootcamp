a08affb8f21f4cb85cc21a20a5e9154374ada605
maykaltenev <michaeltenev@gmail.com>
2022-03-06 21:14:44 +0100

    introducing functions

---
+++ s20-introducing-funtions/app.js
@@ -0,0 +1,45 @@
+// function singSong() {
+//     console.log("DO");
+//     console.log("RE");
+//     console.log("MI");
+// }
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+}
+
+function repeat(str, numTimes) {
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
+
+function add(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return false;
+    }
+    return x + y;
+}
+
+
+
+let die1 = Math.floor((Math.random() * 6) + 1);
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+
