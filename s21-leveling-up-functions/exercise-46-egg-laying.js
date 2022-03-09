2ca7262f4081b2e318ae80e1a721df5a21ab0ddc
maykaltenev <michaeltenev@gmail.com>
2022-03-09 08:54:57 +0100

    exercise-46-egg-laying

---
+++ s21-leveling-up-functions/exercise-46-egg-laying.js
@@ -0,0 +1,5 @@
+// Define an objeect called hen. It should have three properties:
+// - name should be set to "Helen"
+// - eggCount should be set to 0
+// - layAnEgg should be a method which increments the value of eggCount by 1 and returns the string "Egg" 
+// You''ll need to use this
