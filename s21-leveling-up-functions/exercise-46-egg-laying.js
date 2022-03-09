06e05831d8733df5323aa8082fd161418f26a88d
maykaltenev <michaeltenev@gmail.com>
2022-03-09 18:09:26 +0100

    exercise-46-egg-laying

--- s21-leveling-up-functions/exercise-46-egg-laying.js
+++ s21-leveling-up-functions/exercise-46-egg-laying.js
@@ -3,3 +3,16 @@
 // - eggCount should be set to 0
 // - layAnEgg should be a method which increments the value of eggCount by 1 and returns the string "Egg" 
 // You''ll need to use this
+
+let hen = {
+    name: 'Helen',
+    eggCount: 0,
+    layAnEgg: function () {
+        this.eggCount += 1;
+        return 'EGG'
+    }
+}
+
+console.log(hen.layAnEgg());
+console.log(hen.layAnEgg());
+console.log(hen.eggCount);
