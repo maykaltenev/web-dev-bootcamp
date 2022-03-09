47261eb234d0bcab2de4e50160ecbc3c893075fa
maykaltenev <michaeltenev@gmail.com>
2022-03-09 08:25:05 +0100

    s21-leveling-up-functions

---
+++ s21-leveling-up-functions/function-expressions/quiz-7-function-scope.js
@@ -0,0 +1,23 @@
+// let animal = "Giant Pacific Octopus";
+// function observe() {
+//     let animal = "Pajama Squid";
+//     console.log(animal);
+// }
+// observe();
+
+// const creature = "Common Sea Dragon";
+// function scubaDive() {
+//     const creature = "Spanish Dancer"; //A type of sea slug
+//     console.log(creature);
+// }
+// scubaDive();
+
+let deadlyAnimal = "Blue-Ringed Octopus";
+
+function handleAnimal() {
+    let deadlyAnimal = "Scorpionfish";
+    console.log(deadlyAnimal);
+}
+
+handleAnimal();
+console.log(deadlyAnimal)
