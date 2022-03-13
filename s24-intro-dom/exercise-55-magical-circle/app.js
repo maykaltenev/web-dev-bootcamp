1ea311f352aa785ea18c9248516c386d015d08ab
maykaltenev <michaeltenev@gmail.com>
2022-03-13 18:55:14 +0100

    exercise-55-forest-circle

---
+++ s24-intro-dom/exercise-55-magical-circle/app.js
@@ -0,0 +1,5 @@
+let container = document.querySelector('#container');
+container.style.textAlign = 'center';
+let image = document.querySelector('img');
+image.style.width = '150px';
+image.style.borderRadius = '50%';
