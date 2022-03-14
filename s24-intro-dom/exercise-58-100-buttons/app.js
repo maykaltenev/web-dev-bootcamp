b49c8ecceb4431665a92c0d4dc239d4f44d60e97
maykaltenev <michaeltenev@gmail.com>
2022-03-14 20:37:40 +0100

    exercise-100-buttons

---
+++ s24-intro-dom/exercise-58-100-buttons/app.js
@@ -0,0 +1,7 @@
+
+for (let index = 0; index < 100; index++) {
+    let button = document.createElement("button");
+    button.innerText = "Love";
+    let c = document.querySelector("#container");
+    c.appendChild(button);
+}
