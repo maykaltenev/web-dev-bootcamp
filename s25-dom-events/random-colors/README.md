f07681697b9fd3aa69918b9dbd8e444187c46399
maykaltenev <michaeltenev@gmail.com>
2022-03-15 21:40:11 +0100

    s25-dom-events

---
+++ s25-dom-events/random-colors/README.md
@@ -0,0 +1,27 @@
+## No Orange Life Is Gray
+
+![img](./img/find-your-color.png)
+[Test this app yourself](https://maykaltenev.github.io/generate-random-rgba/)
+
+---
+
+#### Navigation
+
+### About Project
+
+This project is for generating a random rgba color! Please find your true color!
+
+### How to use this Project
+
+![demo](.//img/find-color.gif.gif)
+
+##### Developed With
+
+- [x] _HTML5_
+- [x] _CSS3_
+- [ ] _SASS_
+- [ ] _SCSS_
+- [x] _JavaScript_
+- [ ] _React_
+- [ ] _Bootstrap_
+- [ ] _npm_
--- s25-dom-events/random-colors/app.js
+++ s25-dom-events/random-colors/app.js
@@ -4,13 +4,13 @@ const h1 = document.querySelector('h1');
 button.addEventListener('click', function () {
     const newColor = makeRandColor();
     document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
 })
 
 const makeRandColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
+    const a = Math.random();
+    return `rgba(${r}, ${g}, ${b}, ${a.toFixed(2)})`;
 }
 
