18b07c647fa87cdebffa0a22bd7659bf73f4c289
maykaltenev <michaeltenev@gmail.com>
2022-03-18 17:45:43 +0100

    callBack

--- s27-async-javascript/callbacks/app.js
+++ s27-async-javascript/callbacks/app.js
@@ -3,3 +3,9 @@ setTimeout(() => {
     console.log("Here is your data from the server...")
 }, 3000)
 console.log("I AM AT THE END OF THE FILE!")
+
+console.log('I print first!');
+setTimeout(() => {
+    console.log('I print after 3 seconds')
+}, 3000);
+console.log('I print second');
--- s27-async-javascript/callstack/app.js
+++ s27-async-javascript/callstack/app.js
@@ -6,6 +6,6 @@ const isRightTriangle = (a, b, c) => (
     square(a) + square(b) === square(c)
 )
 console.log("BEFORE")
-isRightTriangle(3, 4, 5)
+console.log(isRightTriangle(3, 4, 5));
 
 console.log("DONEEEE!")
