31bf7e5a868bc6b66814b9d289b48acb284ca8b0
maykaltenev <michaeltenev@gmail.com>
2022-03-18 18:07:53 +0100

    callback hell

--- s27-async-javascript/callbackHell/app.js
+++ s27-async-javascript/callbackHell/app.js
@@ -16,7 +16,21 @@
 //         }, 1000)
 //     }, 1000)
 // }, 1000)
-
+// setTimeout(() => {
+//     document.body.style.background = 'red';
+//     setTimeout(() => {
+//         document.body.style.background = 'orange';
+//         setTimeout(() => {
+//             document.body.style.background = 'yellow';
+//             setTimeout(() => {
+//                 document.body.style.background = 'blue';
+//                 setTimeout(() => {
+//                     document.body.style.background = 'purple';
+//                 }, 1000);
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+// }, 1000);
 
 const delayedColorChange = (newColor, delay, doNext) => {
     setTimeout(() => {
@@ -25,18 +39,48 @@ const delayedColorChange = (newColor, delay, doNext) => {
     }, delay)
 }
 
-// STILL A LOT OF NESTING!!!
-delayedColorChange('red', 1000, () => {
+delayedColorChange('olive', 3000, () => {
     delayedColorChange('orange', 1000, () => {
         delayedColorChange('yellow', 1000, () => {
-            delayedColorChange('green', 1000, () => {
-                delayedColorChange('blue', 1000, () => {
-
+            delayedColorChange('blue', 1000, () => {
+                delayedColorChange('red', 1000, () => {
                 })
             })
         })
     })
 });
+
+// pseudo-code:
+
+searchMoviesAPI('amadeus', () => {
+    saveToMyDB(movies, () => {
+        // if it works, run this:
+    }, () => {
+        // if it doesn't work run this
+    })
+}, () => {
+    // if API is down, or request failed
+})
+
+// const delayedColorChange = (newColor, delay, doNext) => {
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = newColor;
+//         doNext && doNext();
+//     }, delay)
+// }
+
+// // STILL A LOT OF NESTING!!!
+// delayedColorChange('red', 1000, () => {
+//     delayedColorChange('orange', 1000, () => {
+//         delayedColorChange('yellow', 1000, () => {
+//             delayedColorChange('green', 1000, () => {
+//                 delayedColorChange('blue', 1000, () => {
+
+//                 })
+//             })
+//         })
+//     })
+// });
 
 
 // searchMoviesAPI('amadeus', () => {
