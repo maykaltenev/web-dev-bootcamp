786941e91f29a3bf0a994c219504f7c89b8c84cb
maykaltenev <michaeltenev@gmail.com>
2022-03-19 09:35:19 +0100

    promises then and catch

--- s27-async-javascript/promisesIntro/app.js
+++ s27-async-javascript/promisesIntro/app.js
@@ -1,28 +1,49 @@
 // THE CALLBACK VERSION
+// const fakeRequestCallback = (url, success, failure) => {
+//     const delay = Math.floor(Math.random() * 4500) + 500;
+//     setTimeout(() => {
+//         if (delay > 4000) {
+//             failure('Connection Timeout :(')
+//         } else {
+//             success(`Here is your fake data from ${url}`)
+//         }
+//     }, delay)
+// }
 const fakeRequestCallback = (url, success, failure) => {
-    const delay = Math.floor(Math.random() * 4500) + 500;
+    const delay = MATH.floor(Math.random() * 4500) + 500;
     setTimeout(() => {
         if (delay > 4000) {
             failure('Connection Timeout :(')
         } else {
-            success(`Here is your fake data from ${url}`)
+            success(`Here is your fake data from ${url}`);
         }
     }, delay)
 }
 // THE PROMISE VERSION 
+// const fakeRequestPromise = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const delay = Math.floor(Math.random() * (4500)) + 500;
+//         setTimeout(() => {
+//             if (delay > 4000) {
+//                 reject('Connection Timeout :(')
+//             } else {
+//                 resolve(`Here is your fake data from ${url}`)
+//             }
+//         }, delay)
+//     })
+// }
 const fakeRequestPromise = (url) => {
     return new Promise((resolve, reject) => {
-        const delay = Math.floor(Math.random() * (4500)) + 500;
+        const deley = Math.floor(Math.random() * (4500)) + 500;
         setTimeout(() => {
-            if (delay > 4000) {
-                reject('Connection Timeout :(')
+            if (deley > 4000) {
+                reject('Connection Timeout :(');
             } else {
-                resolve(`Here is your fake data from ${url}`)
+                resolve(`Here is your fake data from ${url}`);
             }
-        }, delay)
+        }, deley)
     })
 }
-
 
 // fakeRequestCallback('books.com/page1',
 //     function (response) {
@@ -79,25 +100,46 @@ const fakeRequestPromise = (url) => {
 
 // THE CLEANEST OPTION WITH THEN/CATCH
 // RETURN A PROMISE FROM .THEN() CALLBACK SO WE CAN CHAIN!
+// fakeRequestPromise('yelp.com/api/coffee/page1')
+//     .then((data) => {
+//         console.log("IT WORKED!!!!!! (page1)")
+//         console.log(data)
+//         return fakeRequestPromise('yelp.com/api/coffee/page2')
+//     })
+//     .then((data) => {
+//         console.log("IT WORKED!!!!!! (page2)")
+//         console.log(data)
+//         return fakeRequestPromise('yelp.com/api/coffee/page3')
+//     })
+//     .then((data) => {
+//         console.log("IT WORKED!!!!!! (page3)")
+//         console.log(data)
+//     })
+//     .catch((err) => {
+//         console.log("OH NO, A REQUEST FAILED!!!")
+//         console.log(err)
+//     })
+
 fakeRequestPromise('yelp.com/api/coffee/page1')
-    .then((data) => {
-        console.log("IT WORKED!!!!!! (page1)")
-        console.log(data)
-        return fakeRequestPromise('yelp.com/api/coffee/page2')
-    })
-    .then((data) => {
-        console.log("IT WORKED!!!!!! (page2)")
-        console.log(data)
-        return fakeRequestPromise('yelp.com/api/coffee/page3')
-    })
-    .then((data) => {
-        console.log("IT WORKED!!!!!! (page3)")
-        console.log(data)
-    })
-    .catch((err) => {
-        console.log("OH NO, A REQUEST FAILED!!!")
-        console.log(err)
-    })
+    .then(() => {
+        console.log('It worked!!!! (page1)')
+        fakeRequestPromise('yelp.com/api/coffee/page2')
+            .then(() => {
+                console.log('Promise resolved(page2)')
+                fakeRequestPromise('yelp.com/api/coffee/page3')
+                    .then(() => {
+                        console.log('Promise resolved (page3)')
+                    })
+                    .catch(() => {
+                        console.log('Oh.no error(page3)')
+                    })
+            })
+            .catch(() => {
+                console.log('Promise rejected!')
+                console.log('Oh no, error! (page2)')
+            })
 
-
-
+    })
+    .catch(() => {
+        console.log('Oh no, error! (page1)')
+    })
