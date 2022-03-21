60c5dccde3e9768820902a6eb09374315e207325
maykaltenev <michaeltenev@gmail.com>
2022-03-21 21:54:50 +0100

    async

--- s27-async-javascript/creatingPromises/app.js
+++ s27-async-javascript/creatingPromises/app.js
@@ -1,6 +1,26 @@
+// const fakeRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const rand = Math.random();
+//         setTimeout(() => {
+//             if (rand < 0.7) {
+//                 resolve('YOUR FAKE DATA HERE');
+//             }
+//             reject('Request Error!');
+//         }, 1000)
+//     })
+// }
+
+// fakeRequest('/dogs/1')
+//     .then((data) => {
+//         console.log("DONE WITH REQUEST!")
+//         console.log('data is:', data)
+//     })
+//     .catch((err) => {
+//         console.log("OH NO!", err)
+//     })
+
 const fakeRequest = (url) => {
     return new Promise((resolve, reject) => {
-        const rand = Math.random();
         setTimeout(() => {
             if (rand < 0.7) {
                 resolve('YOUR FAKE DATA HERE');
@@ -9,16 +29,14 @@ const fakeRequest = (url) => {
         }, 1000)
     })
 }
-
 fakeRequest('/dogs/1')
     .then((data) => {
-        console.log("DONE WITH REQUEST!")
+        console.log('DONE WITH REQUEST!')
         console.log('data is:', data)
     })
     .catch((err) => {
-        console.log("OH NO!", err)
+        console.log('OH NO!', err)
     })
-
 
 // const delayedColorChange = (newColor, delay, doNext) => {
 //     setTimeout(() => {
