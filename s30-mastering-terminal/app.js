f1ea826d37c2d66f0aa800a0607da1e91d149592
maykaltenev <michaeltenev@gmail.com>
2022-03-26 19:52:23 +0100

    s30-mastering-terminal

---
+++ s30-mastering-terminal/app.js
@@ -0,0 +1,16 @@
+// terminal commands:
+// ls - list all files
+// ls -l - long list
+// ls -a - list hidden files
+// pwd - show current directory
+// mkdir - create a directory
+// touch - create a new item
+// cd - change directory
+// cd .. - go one folder back
+// rm - remove files
+// rmdir - delete folders
+// rm -rf - remove recursive with force
+
+// terminal 
+// a text-based interface to your computer originally a physical object, but now we use software terminal
+// - shell the software running on the terminal
