0823b0cde86ac35906cb004471961d50da1efc16
maykaltenev <michaeltenev@gmail.com>
2022-03-26 21:44:26 +0100

    s32-modules-npm

---
+++ s32-modules-npm-universe/math.js
@@ -0,0 +1,16 @@
+// do we have a specific system or boiler plate how to require variables
+const add = (x, y) => x + y;
+
+const PI = 3.14159;
+
+const square = x => x * x;
+
+exports.square = square;
+exports.PI = PI;
+
+// =========================
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+
+
